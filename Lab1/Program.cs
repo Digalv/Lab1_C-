c387de830a866f434c54143f57d18a61999ff5ae
Smork <daniil.tabakov004@gmail.com>
2023-09-24 16:13:03 +0200

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        Console.WriteLine(Cube(rnd.Next(5000) - rnd.Next(5000), rnd.Next(5000) - rnd.Next(5000)));
+    }
+
+    static double Cube(int num1, int num2)
+    {
+        return Math.Pow((int)num1 + num2, 3);
+    }
+}
